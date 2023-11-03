cbee9e215b481b805610076c3904d3e5082c4732
Y'Kotyk <makurohashami@gmail.com>
2023-11-03 22:39:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<p align="center">
+  <img src="https://i.imgur.com/meePOPU.png" alt="RC_logo" width="100" />
+</p>
+<h1 align="center">Realtor Connect</h1>
+
+---
---
+++ src/test/java/com/kotyk/realtorconnect/RealtorConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotyk.realtorconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtorConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
