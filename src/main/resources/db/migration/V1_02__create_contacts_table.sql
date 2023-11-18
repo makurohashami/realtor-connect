88f61eb33d1f4993bcd74e008daba5cc42c1cf85
Yaroslav Kotyk <makurohashami@gmail.com>
2023-11-18 23:19:10 +0200

    RC-22: moved to spring 3.1.5

--- src/main/resources/db/migration/V1_02__create_contacts_table.sql
+++ src/main/resources/db/migration/V1_02__create_contacts_table.sql
@@ -2,7 +2,7 @@ create table realtors_contacts
 (
     id         bigserial     not null primary key,
     contact    varchar(2048) not null,
-    type_id    smallint      not null,
+    type_id    integer       not null,
     realtor_id bigint        not null
         constraint fk_to_realtor_id
             references realtors_info
