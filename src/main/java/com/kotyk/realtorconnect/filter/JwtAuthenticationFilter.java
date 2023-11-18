50bddf8a7a67a8784abf0209d85bb9ef7c986603
Yaroslav Kotyk <makurohashami@gmail.com>
2023-11-18 21:25:37 +0200

    RC-16: added handler

--- src/main/java/com/kotyk/realtorconnect/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/kotyk/realtorconnect/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,20 @@
 package com.kotyk.realtorconnect.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kotyk.realtorconnect.dto.Error;
 import com.kotyk.realtorconnect.dto.auth.JwtToken;
 import com.kotyk.realtorconnect.entity.user.User;
 import com.kotyk.realtorconnect.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +23,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.Instant;
+
+import static com.kotyk.realtorconnect.util.ApiResponseUtil.wrapError;
 
 @Component
 @RequiredArgsConstructor
@@ -33,15 +42,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         jwt = authHeader.substring(7);
-        JwtToken token = jwtService.parseToken(jwt);
-        if (token.getUsername() != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = new User(token.getUsername(), token.getRole());
-            if (jwtService.isTokenValid(token)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            JwtToken token = jwtService.parseToken(jwt);
+            if (token.getUsername() != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = new User(token.getUsername(), token.getRole());
+                if (jwtService.isTokenValid(token)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException ex) {
+            Error error = new Error(
+                    Instant.now(),
+                    "Bad JWT token. Please try to log in again",
+                    ex.getMessage(),
+                    request.getRequestURI()
+            );
+            ResponseEntity<?> apiResponse = wrapError(error, HttpStatus.UNAUTHORIZED);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write(mapper.writeValueAsString(apiResponse.getBody()));
         }
-        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/kotyk/realtorconnect/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kotyk/realtorconnect/util/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.kotyk.realtorconnect.util.exception;
 
+import com.kotyk.realtorconnect.dto.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +12,9 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
 import java.util.Optional;
+
+import static com.kotyk.realtorconnect.util.ApiResponseUtil.badRequest;
+import static com.kotyk.realtorconnect.util.ApiResponseUtil.wrapError;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,7 +31,18 @@ public class GlobalExceptionHandler {
                 String.format("%s %s", fieldError.getField(), fieldError.getDefaultMessage()),
                 request.getDescription(false)
         );
-        return ResponseEntity.badRequest().body(error);
+        return badRequest(error);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> methodAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+        Error error = new Error(
+                Instant.now(),
+                "Forbidden",
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return wrapError(error, HttpStatus.FORBIDDEN);
     }
 
 }
