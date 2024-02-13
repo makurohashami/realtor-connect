94578f0f7b11ca88a20796d6893d60747ccda684
Yaroslav Kotyk <makurohashami@gmail.com>
2024-02-13 09:17:50 +0200

    RC-36: permission service refactor

--- src/main/java/com/kotyk/realtorconnect/service/PermissionService.java
+++ src/main/java/com/kotyk/realtorconnect/service/PermissionService.java
@@ -2,7 +2,6 @@ package com.kotyk.realtorconnect.service;
 
 import com.kotyk.realtorconnect.entity.realestate.RealEstate;
 import com.kotyk.realtorconnect.entity.realtor.Contact;
-import com.kotyk.realtorconnect.entity.realtor.Realtor;
 import com.kotyk.realtorconnect.entity.user.Permission;
 import com.kotyk.realtorconnect.entity.user.User;
 import com.kotyk.realtorconnect.repository.ContactRepository;
@@ -24,49 +23,44 @@ public class PermissionService {
     private final RealEstateRepository realEstateRepository;
     private final ContactRepository contactRepository;
 
-    @Transactional(readOnly = true)
-    public boolean isSameUser(long id) {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userRepository.findByUsername(username)
+    private User getUser(String username) {
+        return userRepository.findByUsername(username)
                 .orElse(User.builder().id(-1L).build());
-        return user.getId().equals(id);
     }
 
-    @Transactional(readOnly = true)
-    public boolean isRealEstateOwner(long realEstateId) {
-        RealEstate plug = new RealEstate();
-        plug.setRealtor(Realtor.builder().id(-1L).build());
-        RealEstate realEstate = realEstateRepository.findById(realEstateId)
-                .orElse(plug);
-        if (realEstate.getId() == null) {
-            return false;
-        }
-        return isSameUser(realEstate.getRealtor().getId());
-    }
-
-    @Transactional(readOnly = true)
-    public boolean isContactOwner(long contactId) {
-        Contact plug = new Contact();
-        plug.setRealtor(Realtor.builder().id(-1L).build());
-        Contact contact = contactRepository.findById(contactId)
-                .orElse(plug);
-        if (contact.getId() == null) {
-            return false;
-        }
-        return isSameUser(contact.getRealtor().getId());
+    public String getCurrentUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
     public boolean isCurrentHasPermission(Permission permission) {
-        return SecurityContextHolder
-                .getContext()
+        return SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getAuthorities()
                 .contains(new SimpleGrantedAuthority(permission.name()));
     }
 
+
+    @Transactional(readOnly = true)
+    public boolean isSameUser(long id) {
+        return getUser(getCurrentUsername()).getId().equals(id);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isRealEstateOwner(long realEstateId) {
+        Optional<RealEstate> realEstate = realEstateRepository.findById(realEstateId);
+        return realEstate.isPresent() && isSameUser(realEstate.get().getId());
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isContactOwner(long contactId) {
+        Optional<Contact> contact = contactRepository.findById(contactId);
+        return contact.isPresent() && isSameUser(contact.get().getId());
+    }
+
+    @Transactional(readOnly = true)
     public boolean isRealEstatePublic(long realEstateId) {
-        Optional<RealEstate> realEstateOpt = realEstateRepository.findById(realEstateId);
-        return realEstateOpt.map(realEstate -> !realEstate.isPrivate()).orElse(false);
+        Optional<RealEstate> realEstate = realEstateRepository.findById(realEstateId);
+        return realEstate.isPresent() && !realEstate.get().isPrivate();
     }
 
 
