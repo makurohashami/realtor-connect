1137ca1a824595155bc264ad21b955511450e317
Yaroslav Kotyk <makurohashami@gmail.com>
2024-03-30 09:00:27 +0200

    RC-30: grouped APIs

--- src/main/java/com/kotyk/realtorconnect/config/SwaggerConfiguration.java
+++ src/main/java/com/kotyk/realtorconnect/config/SwaggerConfiguration.java
@@ -5,12 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.Setter;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "openapi")
 public class SwaggerConfiguration {
+
+    private Map<String, String[]> groups;
 
     @Bean
     public OpenAPI openApiConfiguration(
@@ -31,6 +40,38 @@ public class SwaggerConfiguration {
                         .version(version)
                         .description(description)
                 );
+    }
+
+    @Bean
+    public GroupedOpenApi allAPIs() {
+        return GroupedOpenApi.builder()
+                .group("All APIs")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userAPIs() {
+        return GroupedOpenApi.builder()
+                .group("User APIs")
+                .pathsToMatch(groups.get("userAPIs"))
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi realtorAPIs() {
+        return GroupedOpenApi.builder()
+                .group("Realtor APIs")
+                .pathsToMatch(groups.get("realtorAPIs"))
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminAPIs() {
+        return GroupedOpenApi.builder()
+                .group("Admin APIs")
+                .pathsToMatch(groups.get("adminAPIs"))
+                .build();
     }
 
 }
